ce0bca456e713d94eedfd9225381e4078495d06d
David Cordero <dcorderoramirez@gmail.com>
2017-03-09 12:42:48 +0100

    Add example reproduce the bug

--- TextFieldFontSize/ViewController.swift
+++ TextFieldFontSize/ViewController.swift
@@ -11,45 +11,28 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var secureTextField: UITextField!
-    @IBOutlet weak var nonSecureTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setUpSecureTextFieldProgramatically()
-        setUpNonSecureTextFieldProgramatically()
         setUpSecureTextFieldFromInterfaceBuilder()
-        setUpNonSecureTextFieldFromIntefaceBuilder()
     }
     
     // MARK: - Private
     
-    private var attributes = [
-        NSFontAttributeName: UIFont.systemFont(ofSize: 30)
-    ]
-    
     func setUpSecureTextFieldProgramatically() {
-        let secureTextField = UITextField(frame: CGRect(x: 100, y: 100, width: 1700, height: 70))
-        secureTextField.isSecureTextEntry = true
-        secureTextField.attributedPlaceholder = NSAttributedString(string: "Secure UITextField programatically created", attributes: attributes)
-        
-        view.addSubview(secureTextField)
-    }
-    
-    func setUpNonSecureTextFieldProgramatically() {
-        let nonSecureTextField = UITextField(frame: CGRect(x: 100, y: 200, width: 1700, height: 70))
-        nonSecureTextField.attributedPlaceholder = NSAttributedString(string: "Non Secure UITextField programatically created", attributes: attributes)
-        
-        view.addSubview(nonSecureTextField)
+        let textField = UITextField(frame: CGRect(x: 100, y: 100, width: 200, height: 70))
+        textField.isSecureTextEntry = true
+        textField.contentVerticalAlignment = .center
+        textField.placeholder = "TextField 1"
+        view.addSubview(textField)
     }
     
     func setUpSecureTextFieldFromInterfaceBuilder() {
+        secureTextField.contentVerticalAlignment = .center
         secureTextField.isSecureTextEntry = true
-        secureTextField.attributedPlaceholder = NSAttributedString(string: "Secure UITextField created from Interface Builder", attributes: attributes)
-    }
-    
-    func setUpNonSecureTextFieldFromIntefaceBuilder() {
-        nonSecureTextField.attributedPlaceholder = NSAttributedString(string: "Non Secure UITextField created from Interface Builder", attributes: attributes)
+        secureTextField.placeholder = "TextField 2"
     }
 }
 
